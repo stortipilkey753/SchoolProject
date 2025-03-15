a69a12ec4ec9453f0896804d610ad155cb2f3a6f
stortipilkey753 <stortipilkey753@gmail.com>
2025-03-15 21:35:17 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schoolproject',
+  templateUrl: './schoolproject.component.html',
+  styleUrls: ['./schoolproject.component.css']
+})
+export class SchoolProjectComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
