3f9b6eb49b6c42a5592fb71d3fc4fe352edebf74
stortipilkey753 <stortipilkey753@gmail.com>
2025-04-26 11:10:19 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,9 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime(7)); // Output: true
